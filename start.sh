d300eeb27f29146250e72d9ce91aa38339ef6c0d
RAMIZ RAEES <RAMIZRAEES@BAGH.MTBC.COM>
2025-12-02 01:54:37 -0500

    Remove start.sh - let nixpacks handle everything

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting Streamlit..."
-exec streamlit run streamlit_app.py --server.port ${PORT:-8501} --server.address 0.0.0.0 --server.headless true --server.enableCORS=false --server.enableXsrfProtection=false
